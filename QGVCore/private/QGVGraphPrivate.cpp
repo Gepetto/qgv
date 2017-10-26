bb33a8368a002e643dbcb9d8a43ce3bec31baa1e
Joseph Mirabel <jmirabel@laas.fr>
2017-10-26 11:36:35 +0200

    Call agclose when clearing the graph

--- QGVCore/private/QGVGraphPrivate.cpp
+++ QGVCore/private/QGVGraphPrivate.cpp
@@ -1,12 +1,15 @@
 #include "QGVGraphPrivate.h"
 
 QGVGraphPrivate::QGVGraphPrivate(Agraph_t *graph)
+  : _graph (NULL)
 {
 	setGraph(graph);
 }
 
 void QGVGraphPrivate::setGraph(Agraph_t *graph)
 {
+  if (_graph != NULL)
+    agclose(_graph);
 	_graph = graph;
 }
 
